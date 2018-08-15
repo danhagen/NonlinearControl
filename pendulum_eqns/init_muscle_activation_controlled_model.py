62d1d1ecc9ab5fdd5ba8cf77db4adc0127868212
danhagen <hagend84@gmail.com>
2018-08-15 13:09:53 -0700

    Changed the return_random_initial_muscle_lengths_and_activations function to only return those muscle lengths that are associated with positive ativations

--- pendulum_eqns/init_muscle_activation_controlled_model.py
+++ pendulum_eqns/init_muscle_activation_controlled_model.py
@@ -1,5 +1,6 @@
 from pendulum_eqns.integrator_backstepping_equations import *
 from pendulum_eqns.initial_tension import *
+from pendulum_eqns.physiology.muscle_params_BIC_TRI import *
 
 MaxStep_Activation = 0.003125 # percentage of positive maximum (1)
 Activation_Bounds = [[0,1],[0,1]]
@@ -17,25 +18,72 @@ def return_random_initial_muscle_lengths_and_activations(InitialTension,**kwargs
 	# L1 = np.linspace(0.5*lo1, 1.5*lo1, 1001)
 	mu1, sigma1 = lo1, 0.1*lo1
 	L1 = np.array(list(sorted(np.random.normal(mu1, sigma1, 1001))))
-	U1 = (
-		InitialTension[0][0]/(F_MAX1*np.cos(α1))
-	    - c_1*k_1*np.log(np.exp((L1/(lo1*L_CE_max_1) - Lr1)/k_1)+1)
-	    ) / (np.exp(-(abs((L1-lo1)/(lo1*ω))**ρ)))
+	U1 = np.array(
+			list(
+				map(
+					lambda l:
+						(InitialTension[0][0]/(F_MAX1*np.cos(α1))
+						- F_PE1_1([0,0,0,0,l,0,0,0])
+						)
+						/ FL(l,lo1)
+					,L1
+				)
+			)
+		)
+	thresh1 = (
+			lo1
+			* L_CE_max_1
+			* (
+				k_1
+				* np.log(
+					np.exp(
+						InitialTension[0][0]
+						/ (F_MAX1*c_1*k_1*np.cos(α1)))
+						- 1
+						)
+					+ Lr1
+				)
+			)
 
 	# L2 = np.linspace(0.5*lo2, 1.5*lo2, 1001)
 	mu2, sigma2 = lo2, 0.1*lo2
 	L2 = np.array(list(sorted(np.random.normal(mu2, sigma2, 1001))))
-	U2 = (
-		InitialTension[1][0]/(F_MAX2*np.cos(α2))
-	    - c_1*k_1*np.log(np.exp((L2/(lo2*L_CE_max_2) - Lr1)/k_1)+1)
-	    ) / (np.exp(-(abs((L2-lo2)/(lo2*ω))**ρ)))
-
+	U2 = np.array(
+			list(
+				map(
+					lambda l:
+						(InitialTension[1][0]/(F_MAX2*np.cos(α2))
+						- F_PE1_2([0,0,0,0,0,l,0,0])
+						)
+						/ FL(l,lo2)
+					,L2
+				)
+			)
+		)
+	thresh2 = (
+			lo2
+			* L_CE_max_2
+			* (
+				k_1
+				* np.log(
+					np.exp(
+						InitialTension[1][0]
+						/ (F_MAX2*c_1*k_1*np.cos(α2)))
+						- 1
+						)
+					+ Lr1
+				)
+			)
+	PositiveIndeces = np.intersect1d(
+						np.where(L1<=thresh1),
+						np.where(L2<=thresh2)
+						)
 	if PlotBool == True:
 		plt.figure(figsize=(10,8))
 		plt.title(r"Viable Initial $l_{m,1}$ and $u_{1}$ Values")
 		plt.xlabel(r"$l_{m,1}$ (m)",fontsize=14)
 		plt.ylabel(r"$u_{1}$",fontsize=14)
-		plt.scatter(L1,U1)
+		plt.scatter(L1[PositiveIndeces],U1[PositiveIndeces])
 		plt.plot([lo1,lo1],[0,1],'0.70',linestyle='--')
 		plt.gca().set_ylim((0,1))
 		plt.gca().set_xticks(
@@ -61,7 +109,7 @@ def return_random_initial_muscle_lengths_and_activations(InitialTension,**kwargs
 		plt.title(r"Viable Initial $l_{m,2}$ and $u_{2}$ Values")
 		plt.xlabel(r"$l_{m,2}$ (m)",fontsize=14)
 		plt.ylabel(r"$u_{2}$",fontsize=14)
-		plt.scatter(L2,U2)
+		plt.scatter(L2[PositiveIndeces],U2[PositiveIndeces])
 		plt.plot([lo2,lo2],[0,1],'0.70',linestyle='--')
 		plt.gca().set_ylim((0,1))
 		plt.gca().set_xticks(
@@ -84,7 +132,12 @@ def return_random_initial_muscle_lengths_and_activations(InitialTension,**kwargs
 			fontsize=12)
 
 		plt.show()
-	return(L1,U1,L2,U2)
+	return( L1[PositiveIndeces],
+			U1[PositiveIndeces],
+			L2[PositiveIndeces],
+			U2[PositiveIndeces]
+			)
+
 
 def find_viable_initial_values(**kwargs):
 	"""
